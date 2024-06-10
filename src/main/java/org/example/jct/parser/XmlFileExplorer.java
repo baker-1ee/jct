a40007a2272c15710d26dd9f26af857ea762c0a5
baker-1ee <ljw722007@gmail.com>
2024-06-10 16:28:09 +0900

    init

---
+++ src/main/java/org/example/jct/parser/XmlFileExplorer.java
@@ -0,0 +1,28 @@
+package org.example.jct.parser;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class XmlFileExplorer {
+
+    public List<File> findXmlFiles(String directory) {
+        File dir = new File(directory);
+        List<File> xmlFiles = new ArrayList<>();
+        findXmlFilesRecursively(dir, xmlFiles);
+        return xmlFiles;
+    }
+
+    private void findXmlFilesRecursively(File dir, List<File> xmlFiles) {
+        if (dir.isDirectory()) {
+            for (File file : Objects.requireNonNull(dir.listFiles())) {
+                if (file.isDirectory()) {
+                    findXmlFilesRecursively(file, xmlFiles);
+                } else if (file.getName().endsWith(".xml")) {
+                    xmlFiles.add(file);
+                }
+            }
+        }
+    }
+}
